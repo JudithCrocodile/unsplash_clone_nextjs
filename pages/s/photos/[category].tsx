ee10c0896dd7999c696640ce5d874a33f7191440
Judith Lee <argentum1218@gmail.com>
2024-09-07 09:45:01 -0400

    feat(category): add category page info insection

--- pages/s/photos/[category].tsx
+++ pages/s/photos/[category].tsx
@@ -12,6 +12,19 @@ export default function Photos({ }) {
   // todo
   // if there is no any photo in this category return 404 page
 
+  const pageIntro = (): React.ReactNode=>{
+    return <div className="py-7">
+      <div className="flex">
+        <div className="min-h-fit flex flex-col justify-start md:justify-end md:basis-[40%] basis-full px-3 md:px-0">
+          <h1 className={'mb-2 text-[28px] font-bold leading-[1.2] capitalize'}>{category}</h1>
+      
+        </div>        
+      </div>
+
+
+    </div>
+
+  }
 
   return (
     <>
@@ -21,8 +34,7 @@ export default function Photos({ }) {
 
       <main
       >
-        <PhotoList propTabName={category as string}></PhotoList>
-        {/* {photoId && <div><Photo photoId={photoId}></Photo></div>} */}
+        <PhotoList pageIntro={pageIntro()} propTabName={category as string}></PhotoList>
 
       </main>
     </>
