1558d27082cf5d1a2836c0e3f28bab2b071f8b84
Judith Lee <argentum1218@gmail.com>
2024-09-11 21:11:16 -0400

    feat(imgUrl): change image url

--- components/photoComponent.tsx
+++ components/photoComponent.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function PhotoComponent( { photo, className, fileId }: { photo?: TypePhoto, className?: string, fileId?: string } ) {
 // export default function AuthorInfo( { id, label, variant, defaultValue, slotProps, value, on }: { id: string, label: string, variant: string, defaultValue: string, slotProps: any } ) {
 
-const imageUrl = `/api/photo/${photo?.fileId || fileId}`
+const imageUrl = `https://unsplash-clone-nextjs.vercel.app/api/photo/${photo?.fileId || fileId}`
 
     return (
         <Image src={imageUrl} alt='photo' className={className} width="100" height="100" style={{width: '100%', height: '100%'}} />
